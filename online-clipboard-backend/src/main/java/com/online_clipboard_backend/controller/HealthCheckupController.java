40c695daf1261285018b46eff51b3d2820b2d087
Shaurya Upadhyay <shaurya01836@gmail.com>
2026-04-19 16:56:21 +0530

    Update HealthCheckupController.java

--- online-clipboard-backend/src/main/java/com/online_clipboard_backend/controller/HealthCheckupController.java
+++ online-clipboard-backend/src/main/java/com/online_clipboard_backend/controller/HealthCheckupController.java
@@ -1,6 +1,5 @@
 package com.online_clipboard_backend.controller;
 
-import com.online_clipboard_backend.repository.ClipRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HealthCheckupController {
 
-    private final ClipRepository clipRepository;
 
     @GetMapping("/health-check")
     public String healthCheck() {
-        // Poking the database to wake it up if it's sleeping (common in free-tier DBs)
-        clipRepository.count(); 
         return "Server and Database are running";
     }
 
